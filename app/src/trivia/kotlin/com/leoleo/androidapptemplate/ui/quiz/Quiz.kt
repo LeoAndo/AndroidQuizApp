71d933a53ab99625e04e400e8a759236bfaa6d83
ando <leo81@outlook.jp>
2023-07-31 23:43:47 +0900

    update

--- app/src/trivia/kotlin/com/leoleo/androidapptemplate/ui/quiz/Quiz.kt
+++ app/src/trivia/kotlin/com/leoleo/androidapptemplate/ui/quiz/Quiz.kt
@@ -4,13 +4,6 @@ import androidx.compose.runtime.Immutable
 import com.leoleo.androidapptemplate.R
 
 @Immutable
-internal data class QuestionData(
-    val messageResId: Int,
-    val answers: List<String>,
-    val answerIndex: Int
-)
-
-@Immutable
 internal enum class Quiz(
     val titleResId: Int,
     val data: List<QuestionData>,
@@ -19,20 +12,66 @@ internal enum class Quiz(
         R.string.quiz_01,
         listOf(
             QuestionData(
-                messageResId = R.string.quiz_01_msg_01,
-                answers = listOf("Java", "PHP", "Ruby", "Go", "Swift"),
+                messageResId = R.string.quiz_01_question_01,
+                answers = listOf(
+                    "George Washington",
+                    "John Adams",
+                    "John Quincy Adams",
+                    "Andrew Johnson",
+                    "Donald John Trump"
+                ),
                 answerIndex = 0
             ),
             QuestionData(
-                messageResId = R.string.quiz_01_msg_02,
-                answers = listOf("PHP", "Swift", "Ruby"),
+                messageResId = R.string.quiz_01_question_02,
+                answers = listOf("Chicago", "Louisiana", "Michigan", "Georgia", "Washington D.C."),
+                answerIndex = 4
+            ),
+            QuestionData(
+                messageResId = R.string.quiz_01_question_03,
+                answers = listOf("Japanese fencing", "McDonald\'s", "Sumo", "MLB"),
+                answerIndex = 3
+            ),
+        )
+    ),
+    Q02(
+        R.string.quiz_02,
+        listOf(
+            QuestionData(
+                messageResId = R.string.quiz_02_question_01,
+                answers = listOf("190", "200", "201", "192", "198", "188"),
+                answerIndex = 4
+            ),
+            QuestionData(
+                messageResId = R.string.quiz_02_question_02,
+                answers = listOf("190", "200", "206", "192", "198", "188", "210"),
+                answerIndex = 2
+            ),
+            QuestionData(
+                messageResId = R.string.quiz_02_question_03,
+                answers = listOf("193", "200", "185"),
+                answerIndex = 0
+            ),
+        ),
+    ),
+    Q03(
+        R.string.quiz_03,
+        listOf(
+            QuestionData(
+                messageResId = R.string.quiz_03_question_01,
+                answers = listOf("Google", "Apple", "Microsoft", "Amazon", "Meta", "Yahoo", "Sony"),
+                answerIndex = 0
+            ),
+            QuestionData(
+                messageResId = R.string.quiz_03_question_02,
+                answers = listOf("Google", "Apple", "Microsoft", "Amazon", "Meta", "Yahoo", "Sony"),
                 answerIndex = 1
             ),
             QuestionData(
-                messageResId = R.string.quiz_01_msg_03,
-                answers = listOf("word", "Xcode", "Figma", "Excel"),
+                messageResId = R.string.quiz_03_question_03,
+                answers = listOf("Google", "Apple", "Microsoft", "Amazon", "Meta", "Yahoo", "Sony"),
                 answerIndex = 2
             ),
-        )
+        ),
     ),
 }
